c70ae55b1795bf628fe4cb09a7f96bb69a17bbff
fweber-de <florian.weber.dd@icloud.com>
2014-06-29 14:53:12 +0200

    validation

--- src/Wallet/DataBundle/Entity/Account.php
+++ src/Wallet/DataBundle/Entity/Account.php
@@ -32,7 +32,7 @@ class Account
     /**
      * @var string
      *
-     * @ORM\Column(name="account_number", type="string", length=255)
+     * @ORM\Column(name="account_number", type="string", length=255, nullable=true)
      */
     private $accountNumber;
 
